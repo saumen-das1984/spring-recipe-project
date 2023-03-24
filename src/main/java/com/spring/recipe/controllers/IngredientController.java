85da43cd7ea938d797440efce1a9298996a92315
saumen das <masterblastersaumen@gmail.com>
2023-03-24 19:28:31 +0530

    View Ingredients

---
+++ src/main/java/com/spring/recipe/controllers/IngredientController.java
@@ -0,0 +1,32 @@
+package com.spring.recipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.recipe.services.RecipeService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class IngredientController {
+	
+	private final RecipeService recipeService;
+
+	public IngredientController(RecipeService recipeService) {
+		this.recipeService = recipeService;
+	}
+	
+	@GetMapping
+	@RequestMapping("/recipe/{recipeId}/ingredients")
+	public String listIngredients(@PathVariable String recipeId, Model model) {
+		log.debug("Retrieve Ingredient List for recipe id "+ recipeId);
+		model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
+		return "recipe/ingredient/list";
+	}
+	
+
+}
